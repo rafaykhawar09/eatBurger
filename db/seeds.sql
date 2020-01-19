5bcd8ca0409a391ead00e06c25975caa589aa279
Rafay Khawar <rafaykhawar09@gmail.com>
2020-01-18 22:22:23 -0800

    changedd file names

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,5 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burgers (name) VALUES ('Cheese Burger');
+INSERT INTO burgers (name) VALUES ('Grilled Chicken');
+INSERT INTO burgers (name, devour) VALUES ('Mc Crispy', false);
+INSERT INTO burgers (name, devour) VALUES ('Zinger Stacker', false);
+INSERT INTO burgers (name, devour) VALUES ('Mighty Zinger', false);
