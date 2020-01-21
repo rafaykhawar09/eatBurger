81b64956ae90ba92a3d778e9c482c35684d5133b
Rafay Khawar <rafaykhawar09@gmail.com>
2020-01-21 12:14:40 -0800

    changed

--- config/connection.js
+++ config/connection.js
@@ -7,13 +7,15 @@ require("dotenv").config();
 if(process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL)
 }
-connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "burger_db"
-});
+else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "burger_db"
+  });
+}
 
 // Make connection.
 connection.connect(function(err) {
